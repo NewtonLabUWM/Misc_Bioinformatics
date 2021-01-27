3c415e6de22576807e12d11237ae6b3f3c175858
Lou LaMartina <48258356+loulanomics@users.noreply.github.com>
2021-01-27 16:33:48 -0600

    Add files via upload

---
+++ cutadapt/v4_cutadapt.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+
+#########################################
+### trimming primer sequences from
+### regions V4 of 16S rRNA gene sequences
+### Lou LaMartina, Sep 1 2020
+#########################################
+
+
+# be in directory where raw reads are stored.
+
+# trimmed reads will be sorted into new directories,
+# ./fastqF and ./fastqR, inside ./cutadapt.
+
+# output for each file will be stored in a text file.
+
+
+mkdir cutadapt
+
+for fileR1 in *_R1*; do
+	fileR2=${fileR1/R1/R2}
+	printf "\nTrimming ${fileR1} and ${fileR2} . . .\n"
+	cutadapt -g "GTGCCAGCMGCCGCGGTAA" -G "GGACTACHVGGGTWTCTAAT " \
+	-m 50 -o cutadapt/$fileR1 -p cutadapt/$fileR2 \
+	$fileR1 $fileR2 >> v4_cutadapt_output.txt
+done
+
+cd cutadapt
+
+mkdir fastqF
+mkdir fastqR
+
+mv *R1* fastqF
+mv *R2* fastqR
