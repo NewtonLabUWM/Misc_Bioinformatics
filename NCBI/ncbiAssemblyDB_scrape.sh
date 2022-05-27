f2b6766245bdc9eae059029cab39b296636b19a5
Lou LaMartina <48258356+loulanomics@users.noreply.github.com>
2022-05-26 20:59:28 -0500

    Update ncbiAssemblyDB_scrape.sh

--- NCBI/ncbiAssemblyDB_scrape.sh
+++ NCBI/ncbiAssemblyDB_scrape.sh
@@ -4,44 +4,34 @@
 ######################################
 ### extract biosample information from 
 ### list of NCBI assembly IDs
-### Lou LaMartina, May 18 2022
+### Lou LaMartina, May 26 2022
 ######################################
 
 
 # conda install -c bioconda entrez-direct
+#			 new bffffff ^
 
 
-# assembly IDs - extracted from file names from NCBI contigs
+# read list of assembly IDs
 input="assemblies.txt"
+assembs=$(cat $input)
 
 
+# for each assembly
+for assemb in $assembs; do
 
-# scrape the assembly database webpage for
-while IFS= read -r assemb; do
-	lynx -dump https://www.ncbi.nlm.nih.gov/assembly/$assemb > tmp.txt
-	link=$(grep "https://www.ncbi.nlm.nih.gov/biosample?LinkName=assembly_biosample&from_uid=" tmp.txt)
+	# get its biosample ID
+	bs=$(esearch -db assembly -query $assemb | efetch -format docsum | xtract -pattern DocumentSummary -element BioSampleAccn)
 
-	# biosample IDs
-	smp=$(lynx -dump $link | grep -A1 "Identifiers" | grep -v "Identifiers" | cut -f1 -d";" | sed 's/BioSample: //g')
-	
-	# bioproject IDs
-	bp=$(lynx -dump $link | grep "PRJ" | sed -e 's/\[[^][]*\]//g' | awk '{$1=$1};1' | cut -d" " -f1)
-	echo $bp $smp $assemb >> bioprojects.txt
-	rm tmp.txt
-done < "$input"
+	# bioproject ID
+	bp=$(esearch -db assembly -query $assemb | efetch -format docsum | xtract -pattern DocumentSummary -element BioprojectAccn)
 
+	# title / organism name hopefully
+	org=$(esearch -db biosample -query $bs | efetch -format docsum | xtract -pattern DocumentSummary -element Title)
 
+    	# save all biosample data
+    	esearch -db biosample -query $bs | efetch -format docsum > ./Metadata/all-$assemb.txt
 
-# with the bioproject IDs, get 
-bps=$(cut -d" " -f1 bioprojects.txt | grep "PRJ"| uniq)
-
-for bp in $bps; do
-
-	# sample information (biosample attributes)
-	esearch -db bioproject -query $bp |\
-	elink -target sra | efetch -format docsum |\
-	xtract -pattern SampleData -element Attribute > ./Metadata/$bp-attributes.txt
-
-	# sequence file information (SRA metadata)
-	esearch -db sra -query $bp | efetch -format runinfo > ./Metadata/$bp-metadata.txt
+    # save select biosample data
+    echo $bp $bs $assemb $org > ./Metadata/sum-$assemb.txt
 done
